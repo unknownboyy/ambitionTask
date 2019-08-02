b8f54a100a75e1fe16b6842b2ff96f881c203dec
Ankit Agrawal <ankitagrawal11b@gmail.com>
2019-08-03 02:19:10 +0530

    With Encryption

--- fileUtils.py
+++ fileUtils.py
@@ -3,6 +3,18 @@ import json
 from encryptionUtils import encrypt, decrypt
 from caseConstants import LOGIN_FILE, ENCRYPTION_KEY, JOURNAL_FILE
 
+def readFile(file_name):
+    file_data = open(file_name,"r")
+    data = decrypt( file_data.read(), ENCRYPTION_KEY )
+    file_data.close()
+    return json.loads(data)
+
+def writeFile(file_name,data):
+    data = json.dumps(data)
+    data = encrypt(data,ENCRYPTION_KEY)
+    file = open(file_name,"w")
+    file.write(data)
+    file.close()
 
 def getEmptyUserFileData():
     users = dict()
@@ -11,7 +23,7 @@ def getEmptyUserFileData():
 def ensureUsersFile():
     if not os.path.isfile(LOGIN_FILE):
         file = open(LOGIN_FILE,"w")
-        json.dump(getEmptyUserFileData(),file)
+        file.write(encrypt(json.dumps(getEmptyUserFileData()),ENCRYPTION_KEY))
         file.close()
 
 def getEmptyJournalFileData():
@@ -21,15 +33,13 @@ def getEmptyJournalFileData():
 def ensureJournalsFile():
     if not os.path.isfile(JOURNAL_FILE):
         file = open(JOURNAL_FILE,"w")
-        json.dump(getEmptyJournalFileData(),file)
+        file.write(encrypt(json.dumps(getEmptyJournalFileData()),ENCRYPTION_KEY))
         file.close()
 
 
 def getUser(email,password):
     ensureUsersFile()
-    file_data = open(LOGIN_FILE,"r")
-    users = json.load(file_data)
-    file_data.close()
+    users = readFile(LOGIN_FILE)
     if email in users and users[email] == password:
         return email
     else:
@@ -37,38 +47,27 @@ def getUser(email,password):
 
 def setUser(email,password):
     ensureUsersFile()
-    file_data = open(LOGIN_FILE,"r")
-    users = json.load(file_data)
-    file_data.close()
-    file_data = open(LOGIN_FILE,"w")
+    users = readFile(LOGIN_FILE)
     users[email] = password
-    json.dump(users,file_data)
+    writeFile(LOGIN_FILE,users)
 
 def getJournals(user):
     ensureJournalsFile()
-    file_data = open(JOURNAL_FILE,"r")
-    journals = json.load(file_data)
-    file_data.close()
+    journals = readFile(JOURNAL_FILE)
     user_journals = journals.get(user,[])
     return user_journals
 
 def saveNewJournal(user,journal):
     ensureJournalsFile()
-    file_data = open(JOURNAL_FILE,"r")
-    journals = json.load(file_data)
-    file_data.close()
-    file_data = open(JOURNAL_FILE,"w")
+    journals = readFile(JOURNAL_FILE)
     user_journals = journals.get(user,[])
     user_journals.append({"TITLE":journal[0],"LOGS":journal[1]})
     if len(user_journals)>50:
         user_journals.pop(0)
     journals[user] = user_journals
-    json.dump(journals,file_data)
-    file_data.close()
+    writeFile(JOURNAL_FILE,journals)
 
 def reachedUserLimit():
     ensureUsersFile()
-    file_data = open(LOGIN_FILE,"r")
-    users = json.load(file_data)
-    file_data.close()
+    users = readFile(LOGIN_FILE)
     return len(users)<10
